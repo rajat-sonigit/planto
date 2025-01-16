a63ecca21de097f78f0fc148af0ecf94f757613d
Rajat Soni <98265341+rajat-sonigit@users.noreply.github.com>
2025-01-16 20:04:07 +0530

    Update bottom_menu.dart

--- lib/data/bottom_menu.dart
+++ lib/data/bottom_menu.dart
@@ -4,7 +4,6 @@ class BottomMenu {
 
   BottomMenu(this.id, this.imagePath);
 }
-
 List<BottomMenu> bottomMenu = [
   BottomMenu(0, 'assets/icons/home.png'),
   BottomMenu(1, 'assets/icons/heart.png'),
